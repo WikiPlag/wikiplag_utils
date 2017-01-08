27a3c8c2a3b0d7f632087c016fa97764547a2abe
maiktheknife <maiktheknife@gmail.com>
2017-01-08 14:42:40 +0100

    make Document.scala a case class to support Scalatra Default JSON Handling

--- src/main/scala/de/htw/ai/wikiplag/model/Document.scala
+++ src/main/scala/de/htw/ai/wikiplag/model/Document.scala
@@ -1,18 +1,9 @@
 package de.htw.ai.wikiplag.model
 
-import com.mongodb.DBObject
-
 /**
   * Created by chris on 13.11.2016.
   */
-class Document(val id: Long = Long.MinValue,
-               val title: String = "",
-               val text: String = "",
-               val viewIndex:List[(Int,Int,Int)] = List.empty[(Int,Int,Int)]) extends Serializable {
-
-//  val id: Long = lId
-//  val title: String = sTitle
-//  val text: String = sText
-//  val viewIndex:List[(Int, Int, Int)] = viewInd
-
-}
+case class Document(id: Long = Long.MinValue,
+                    title: String = "",
+                    text: String = "",
+                    viewIndex: List[(Int, Int, Int)] = List.empty[(Int, Int, Int)]) extends Serializable
